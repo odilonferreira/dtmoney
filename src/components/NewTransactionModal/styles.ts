30fe8dab5857e68b1c375aec9e8f593f883c2343
Odilon Ferreira <odilon.aferreira@gmail.com>
2022-02-23 23:14:57 -0300

    installing polished for styling transactions

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { darken } from "polished";
 
 export const Container = styled.form`
   h2 {
@@ -63,6 +64,12 @@ export const TransactionTypeContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+
+    transition: border-color 0.2s;
+
+    &:hover {
+      border-color: ${darken(0.1, "#d7d7d7")};
+    }
 
     img {
       width: 20px;
